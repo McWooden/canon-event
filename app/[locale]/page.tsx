77195d4a24e15d0a1a5a01866d9ec5455daf0802
McWooden <garryf7887@gmail.com>
2025-12-18 23:29:18 +0700

    katanya lagi maintance

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,23 +1,28 @@
 
-import Hero from '@/app/components/Home/Hero';
-import Brands from '@/app/components/Home/Brands';
-import Footer from '@/app/components/Footer';
-import CallToAction from '@/app/components/Home/CallToAction';
-import Articles from '@/app/components/Home/Articles';
-import Features from '@/app/components/Home/Features';
-import { QuotesOne, QuotesTwo} from '@/app/components/Home/Quotes';
+// import Hero from '@/app/components/Home/Hero';
+// import Brands from '@/app/components/Home/Brands';
+// import Footer from '@/app/components/Footer';
+// import CallToAction from '@/app/components/Home/CallToAction';
+// import Articles from '@/app/components/Home/Articles';
+// import Features from '@/app/components/Home/Features';
+// import { QuotesOne, QuotesTwo} from '@/app/components/Home/Quotes';
 
 export default async function Page() {
 
   return <main>
-    <Hero/>
+    <section className="flex flex-col items-center justify-center min-h-screen mt-0 gap-8">
+        <h1 className="text-9xl">🏗️🚧</h1>
+        <p className='text-base lg:text-3xl leading-[25px] lg:leading-[50px]'>307</p>
+        <p className='text-base lg:text-3xl leading-[25px] lg:leading-[50px]'>Watch out! Still On Construction</p>
+    </section>
+    {/* <Hero/>
     <Brands/>
     <QuotesOne/>
     <Features/>
     <Articles/>
     <QuotesTwo/>
     <CallToAction/>
-    <Footer/>
+    <Footer/> */}
   </main>
 }
 
