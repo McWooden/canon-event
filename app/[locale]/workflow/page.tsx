8e202ccce1438398dc301c7069811d0bdd940a45
McWooden <garryf7887@gmail.com>
2025-12-18 12:07:19 +0700

    on progress workflow

---
+++ app/[locale]/workflow/page.tsx
@@ -0,0 +1,6 @@
+export default function Page() {
+    return <section className="flex flex-col items-center justify-center min-h-screen mt-0 gap-8">
+        <h1 className="text-9xl">🏗️🚧</h1>
+        <p className='text-base lg:text-3xl leading-[25px] lg:leading-[50px]'>Watch out! Still On Construction</p>
+    </section>
+}
